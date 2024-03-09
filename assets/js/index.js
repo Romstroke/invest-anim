e87fd5ffedd503ea5f73b9acf1e9d26fdd47c476
Romina <rominchuela@gmail.com>
2024-03-09 10:31:39 -0300

    Carga inicial

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+// alert('hola')
+
+// En esta prueba deberás crear una aplicación web que permita agregar en una tabla los animales en investigación, utilizando el paradigma POO para la creación de las instancias que representen los diferentes animales con sus atributos y métodos. 
+
+//El formulario debe cumplir la función de registrar a los animales en la tabla de la izquierda.  
+
+// Deberás programar la funcionalidad para agregar al animal y mostrarlo en la tabla de animales en investigación, junto con un botón que reproduzca el audio correspondiente al sonido que emite el animal agregado. 
+
+// Al dar click en las imágenes de los animales debe aparecer una ventana modal con el detalle registrado del animal. 
+
+//Cada método debe reproducir un audio con el sonido que emite cada animal. 
+
+//Utiliza todos los métodos de los Arrays que creas conveniente en las diferentes situaciones con las que te encontrarás en el desarrollo de esta prueba. 
+
+//Utilizar la etiqueta “audio” de HTML. 
+
+// 1. Crear las clases representadas en el diagrama implementando la herencia indicada. (2 Puntos) 
+
+// 5. Dividir el código en módulos  (2 Puntos) 
+
