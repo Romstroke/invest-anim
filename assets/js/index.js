fac98edcf0e8fb12fab3939f16d772338a2a49af
Romina <rominchuela@gmail.com>
2024-03-10 15:18:36 -0300

    Actualizar bootstrap, añadir fontawesome, orden código y avance funcionalidad

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,6 @@
-// alert('hola')
-
-// En esta prueba deberás crear una aplicación web que permita agregar en una tabla los animales en investigación, utilizando el paradigma POO para la creación de las instancias que representen los diferentes animales con sus atributos y métodos. 
-
-//El formulario debe cumplir la función de registrar a los animales en la tabla de la izquierda.  
-
-// Deberás programar la funcionalidad para agregar al animal y mostrarlo en la tabla de animales en investigación, junto con un botón que reproduzca el audio correspondiente al sonido que emite el animal agregado. 
-
-// Al dar click en las imágenes de los animales debe aparecer una ventana modal con el detalle registrado del animal. 
-
-//Cada método debe reproducir un audio con el sonido que emite cada animal. 
-
 //Utiliza todos los métodos de los Arrays que creas conveniente en las diferentes situaciones con las que te encontrarás en el desarrollo de esta prueba. 
 
 //Utilizar la etiqueta “audio” de HTML. 
-
 
 // 1. Crear las clases representadas en el diagrama implementando la herencia indicada. (2 Puntos) 
 // 5. Dividir el código en módulos  (2 Puntos) 
@@ -39,6 +26,7 @@
 //CLASE ANIMAL PADRE
 class Animal {
     constructor(nombre, edad, img, comentarios, sonido) {
+        //DESPUES ESTAS PROPIEDADES TIENEN QUE SER PRIVADAS
         this.nombre = nombre;
         this.edad = edad;
         this.img = img;
@@ -58,7 +46,7 @@ class Animal {
         return this.img;
     }
 
-    get Comentarios() {   //tambien tiene un setter para cambiar comentario
+    get Comentarios() {   //tambien tiene un setter para cambiar comentario porque si es privado, necesito el getter también
         return this.comentarios;
     }
 
@@ -66,57 +54,46 @@ class Animal {
         return this.sonido;
     }
 
-}
+    set Comentarios(nuevo_comentario) {
+        this.comentarios = nuevo_comentario;
+    }
 
-//sin default tendris que importarlo con el {nombrearchivo todos los nombres} y es porque si no, toma el default
-// export {}
+}
 
 //CLASE LEON
 class Leon extends Animal {
     constructor() {
         super()
     }
+    //Cada método debe reproducir un audio con el sonido que emite cada animal.
+    rugir() {
+        // ${audio.setAttribute("src", `"assets/sounds/${animalEncontrado.sonido}"`)}
+        const audioElement = `assets/sounds/${this.sonido}`;
+        console.log(audioElement)
+        audioElement.play();
+    }
 }
 
 console.log({ Leon })
-// console.log([...Leon])
 
-//usar evento submit de formulario para mandarlo a la tabla
-
-//importar al tiro aqui 
-
-//FUNCION IIFE
+//FUNCION IIFE (también va en otro modulo)
 const animales = (() => {
-    // const url = "http://localhost:5500/animales.json"; //si es de github hay que ponerle el enlace de github dijo luxo
-    const url = 'assets/js/animales.json'; //esta no funciona al correrla con node, la 83 si
+    // const url = "http://localhost:5500/animales.json";
+    const url = './animales.json'; //esta no funciona al correrla con node, la anterior si
     const getData = async () => {
         const res = await fetch(url);
-        const datos = await res.json();
+        const datos = await res.json(); // aqui llegan los datos: name, imagen, sonido
         console.log(datos)
         // rellenarImagen(datos)
-        return datos;
+        return datos; // retornar datos
     };
     return { getData };
 })();
 
-const datos = await animales.getData()
-
-console.log(await animales.getData())
-console.log(animales.getData)
-console.log(datos)
-
-//DEL JSON LLEGA NAME, IMAGEN, SONIDO
-
-// console.log({animales})
-//   export default animales;
-
-//la funcion ife tambien seria un modulo
-//dividir
-
-//   const url = 'assets/js/animales.json';
+const datos = await animales.getData() //guardar lo que retorna la función en una constante
+console.log('datos fuera de iife',datos) //tenemos los datos afuera
 
 //TOMAR VALOR DE INPUTS
-
 const selectAnimal = document.getElementById('animal');
 const selectEdad = document.getElementById('edad');
 const comentario = document.getElementById('comentarios');
@@ -133,60 +110,63 @@ selectAnimal.addEventListener('change', () => {
     const animalEncontrado = datos.animales.find(animal => animal.name === selectAnimal.value);
     console.log(animalEncontrado);
     const inyectado = `<img src="assets/imgs/${animalEncontrado.imagen}" width="150px" >`
-    contenedorImagen.innerHTML= inyectado;
+    contenedorImagen.innerHTML = inyectado;
 
-    btnRegistrar.addEventListener('click', () =>{
-    console.log(animalEncontrado.sonido);
+    //usar evento submit de formulario para mandarlo a la tabla
+    btnRegistrar.addEventListener('click', () => {
+        console.log(animalEncontrado.sonido);
+        console.log(animalesInstanciados)
+        let contenidoTabla = '';
+        // Iterar sobre el array de animales instanciados y generar la salida HTML para cada uno
+        animalesInstanciados.forEach(animalInst => {
+            contenidoTabla += `
+            <div>
+                <img src="assets/imgs/${animalInst.imagen}" alt="${animalInst.nombre}" width="150px">
+                <button class="btn btn-secondary btn-sm">
+                    <i class="fa-solid fa-volume-high"></i>
+                </button>
+            </div>
+        `;
+        });
 
-        tabla.innerHTML= `
-        ${inyectado}
-        ${audio.setAttribute("src",`"assets/sounds/${animalEncontrado.sonido}"`)}
-        `
+        tabla.innerHTML = contenidoTabla; // Agregar el contenido generado a la tabla
     });
 
-
+    //EDAD ANIMAL
+    selectEdad.addEventListener('change', () => {
+        console.log(selectEdad.value);  //AQUI LLEGA EL VALOR DEL SELECT
+    });
+    
+    //COMENTARIO
+    comentario.addEventListener('input', () => {
+        console.log(comentario.value); //AQUI LLEGA EL VALOR DEL INPUT
+    });
 
     console.log(typeof (selectAnimal.value)) //AQUI LLEGA EL VALOR DEL SELECT
     instanciarAnimal(selectAnimal.value)
 });
 
-selectEdad.addEventListener('change', () => {
-    console.log(selectEdad.value);  //AQUI LLEGA EL VALOR DEL SELECT
-});
+// //EDAD ANIMAL
+// selectEdad.addEventListener('change', () => {
+//     console.log(selectEdad.value);  //AQUI LLEGA EL VALOR DEL SELECT
+// });
 
-comentario.addEventListener('input', () => {
-    console.log(comentario.value); //AQUI LLEGA EL VALOR DEL INPUT
-});
-
-
-
+// //COMENTARIO
+// comentario.addEventListener('input', () => {
+//     console.log(comentario.value); //AQUI LLEGA EL VALOR DEL INPUT
+// });
 
 //tengo que guardar los animales en un array
+let animalesInstanciados = [];
 
 //FUNCION QUE INSTANCIA UN ANIMALES SEGUN SELECT
-function instanciarAnimal(valorSelect) {
-    let animal = new Animal(valorSelect);
-    console.log('estoy fuera de los if :', animal)
+//nombre, edad, img, comentarios, sonido
+function instanciarAnimal(valorSelect, edad, img, comentarios, sonido) {
+    const nuevoAnimal = new Animal(valorSelect, edad, img, comentarios, sonido);
+    //usando metodo push ✔
+    animalesInstanciados.push(nuevoAnimal)
+    console.log('estoy fuera de los if :', animalesInstanciados)
 }
 
-//RELLENANDO CONTENEDOR IMAGEN
 
-// function rellenarImagen(contenedorImagen,animales){
-
-//     if(selectAnimal.value = 'Leon'){ //por qué no tiene tilde en consola y en select si?
-//        contenedorImagen.innerHTML = animales[0].imagen
-//     }
-// }
-
-// rellenarImagen()
-
-// function rellenarImagen(contenedorImagen, animales, nombreAnimalSeleccionado) {
-//     const animalSeleccionado = datos.animales.find(animal => animal.name === nombreAnimalSeleccionado);
-//     console.log(animalSeleccionado)
-//     if (animalSeleccionado) {
-//         contenedorImagen.innerHTML = `<img src="${animalSeleccionado.imagen}" alt="${nombreAnimalSeleccionado}">`;
-//     } else {
-//         contenedorImagen.innerHTML = "Animal no encontrado";
-//     }
-// }
 
